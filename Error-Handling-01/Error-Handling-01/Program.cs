243897cf2e18c83c37c70c99972a8d87bcdc5cd4
Ozgur <ozgurozvaris@hotmail.com>
2026-02-13 02:26:06 +0300

    Error-Handling

---
+++ Error-Handling-01/Error-Handling-01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Error_Handling_01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int num = 10;
+                int result = num / 0; // This will cause a divide by zero error
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message); // Handle the error
+            }
+            finally
+            {
+                Console.WriteLine("This will run regardless of whether an error occurred or not.");
+            }
+        }
+
+    }
+}
---
+++ Error-Handling-02/Error-Handling-02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Error_Handling_02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int age;
+
+            Console.Write("Please enter your age: ");
+            string input = Console.ReadLine(); // Kullanıcıdan giriş al
+
+            // int.TryParse ile girişin geçerli bir tam sayı olup olmadığını kontrol et
+            if (int.TryParse(input, out age))
+            {
+                Console.WriteLine($"Your age is {age}. Validation successful!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
