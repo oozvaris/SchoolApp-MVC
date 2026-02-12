644091b2f325e22b2bf874bab1956a74764fc0ff
Ozgur <ozgurozvaris@hotmail.com>
2026-02-13 00:03:33 +0300

    Car-Class-01 added

---
+++ Console-Car-Class-01/Console-Car-Class1/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Car_Class1
+{
+    public class Car
+    {
+        // Properties (data)
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; private set; }
+
+        // Constructor
+        public Car(string brand, string color)
+        {
+            Brand = brand;
+            Color = color;
+            Speed = 0;
+        }
+
+        // Methods (behavior)
+        public void Start()
+        {
+            Console.WriteLine($"{Brand} ({Color}) is starting...");
+        }
+
+        public void Accelerate(int amount)
+        {
+            Speed += amount;
+            Console.WriteLine($"{Brand} accelerated by {amount}. Speed = {Speed} km/h");
+        }
+
+        public void Brake(int amount)
+        {
+            Speed -= amount;
+            if (Speed < 0) Speed = 0;
+            Console.WriteLine($"{Brand} braked by {amount}. Speed = {Speed} km/h");
+        }
+
+        public void Stop()
+        {
+            Speed = 0;
+            Console.WriteLine($"{Brand} stopped. Speed = {Speed} km/h");
+        }
+    }
+
+}
---
+++ Console-Car-Class-01/Console-Car-Class1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Console_Car_Class1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Create objects from the class
+        Car car1 = new Car("Toyota", "Red");
+        Car car2 = new Car("BMW", "Black");
+
+        // Use the objects and write to console
+        Console.WriteLine("=== Car 1 ===");
+        car1.Start();
+        car1.Accelerate(30);
+        car1.Brake(10);
+        car1.Stop();
+
+        Console.WriteLine("\n=== Car 2 ===");
+        car2.Start();
+        car2.Accelerate(50);
+        car1.Accelerate(10);
+        car2.Brake(70);
+        car1.Brake(10);
+        car2.Stop();
+
+        Console.Read();
+    }
+}
