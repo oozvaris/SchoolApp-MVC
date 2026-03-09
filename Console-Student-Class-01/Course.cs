481bfdadd5b29da554ffdf25900ceb72d610ac3e
Ozgur <ozgurozvaris@hotmail.com>
2026-03-10 00:03:46 +0300

    Console App  added

---
+++ Console-Student-Class-01/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Student_Class_01
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public string CourseCode { get; set; }
+        public int CourseCredit { get; set; }
+    }
+}
---
+++ Console-Student-Class-01/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Student_Class_01
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+
+        public string StudentSurname { get; set; }
+
+        public string StudentEmail { get; set; }
+
+    }
+}
