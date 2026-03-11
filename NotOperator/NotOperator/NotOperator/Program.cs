4359d435ecd78e8cc1abfe692abce147f9815219
Ozgur <ozgurozvaris@hotmail.com>
2026-03-11 22:11:59 +0300

    Not Operator Added

---
+++ NotOperator/NotOperator/NotOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NotOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isActive = false;
+
+            if (!isActive)
+            {
+                Console.WriteLine("The system is not active.");
+            }
+            else
+            {
+                Console.WriteLine("The system is active.");
+            }
+        }
+    }
+}
